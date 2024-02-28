5ac7a06075ead9d40fa0c0d5aac2a796e77f9657
sergey-komar <kursev81@yandex.ru>
2024-02-28 19:59:42 +0300

    4 page desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,40 @@
 $(function () {
+  $(".slider-block__inner").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+  });
 
-})
+  $(".reviews-slider__inner").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    appendArrows: '.reviews-slider__arrows',
+  });
+});
 
-window.addEventListener('DOMContentLoaded', () => {
-    const headerSerch = document.querySelector('.header-bottom__search');
-    const headerSearchOpen = document.querySelector('.header-bottom__box-img');
-    const headerSearchClose = document.querySelector('.header-bottom__form-close');
+window.addEventListener("DOMContentLoaded", () => {
+  const headerSerch = document.querySelector(".header-bottom__search");
+  const headerSearchOpen = document.querySelector(".header-bottom__box-img");
+  const headerSearchClose = document.querySelector(
+    ".header-bottom__form-close"
+  );
 
-    headerSearchOpen.addEventListener('click', () => {
-        headerSerch.classList.add('searh-hidden');
-    });
+  headerSearchOpen.addEventListener("click", () => {
+    headerSerch.classList.add("searh-hidden");
+  });
 
-    headerSearchClose.addEventListener('click', () => {
-        headerSerch.classList.remove('searh-hidden');
-    });
+  headerSearchClose.addEventListener("click", () => {
+    headerSerch.classList.remove("searh-hidden");
+  });
+
+  const text = document.querySelector(".text p");
+  text.innerHTML = text.innerText
+    .split("")
+    .map(
+      (char, i) =>
+        ` <span style="transform: rotate(${i * 13.9}deg);">${char}</span>`
+    )
+    .join("");
 });
